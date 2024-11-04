6a21f1ace8f0331315a87d2309f4f2da8fd8a90a
krosta12 <kostailusenko911@gmail.com>
2024-11-04 17:35:02 +0200

    React component system

---
+++ src/js/componentLoader.js
@@ -0,0 +1,22 @@
+let components = ["header", "footer"];
+
+function loadComponents() {
+  components.forEach(async (el) => {
+    try {
+      let response = await fetch(`http://127.0.0.1:5500/src/html/${el}.html`);
+
+      const htmlContent = await response.text();
+      const targetElement = document.querySelector(el);
+      if (targetElement) {
+        targetElement.innerHTML = htmlContent;
+      } else {
+        console.error(`Element <${el}> not found in document.`);
+      }
+    } catch (error) {
+      console.error(error);
+      alert("404 - Component not found");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loadComponents);
